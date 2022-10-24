fb097e43076499831b206e914b2eb11ffcd8fac1
Your Name <you@example.com>
2022-10-24 20:36:43 +0530

    cors

--- controller/api/deviceController.js
+++ controller/api/deviceController.js
@@ -40,7 +40,7 @@ exports.getDevicesOfUser = (req, res, next) => {
 exports.updateDeviceLocation = async (req, res, next) => {
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(501).json({ errors: errors.array() });
    }
    try {
       const user = req.body.user;
