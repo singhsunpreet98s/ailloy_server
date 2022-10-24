61f4115306b56d3baeae83fe72876202e79142b2
Your Name <you@example.com>
2022-10-24 22:15:14 +0530

    bug fixes

--- controller/api/deviceController.js
+++ controller/api/deviceController.js
@@ -1,35 +1,44 @@
 const Device = require('../../models/device');
 const { validationResult } = require('express-validator');
-exports.addDevice = (req, res, next) => {
+exports.addDevice = async (req, res, next) => {
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
    }
-   const { imei, name, expirationDate } = req.body;
-   const device = new Device({
-      imei: imei,
-      name: name,
-      expirationDate: expirationDate,
-      addedBy: req.body.user._id,
-   });
-   device.save();
-   res.status(201).json({
-      status: 1,
-      data: {
-         id: device._id,
-         name: device.name,
-         imei: device.imei,
-         active: device.active,
-         isDeleted: device.isDeleted,
-         lastValidLatitude: null,
-         lastValidLongitude: null,
-         engineOnAt: null,
-         engineOffAt: null,
-         engineStatusChangedAt: null,
-         speed: device.speed,
-         engine: device.engine
-      }
-   });
+   try {
+      const { imei, name, expirationDate } = req.body;
+      const device = new Device({
+         imei: imei,
+         name: name,
+         expirationDate: expirationDate,
+         addedBy: req.body.user._id,
+      });
+      await device.save();
+      res.status(201).json({
+         status: 1,
+         data: {
+            id: device._id,
+            name: device.name,
+            imei: device.imei,
+            active: device.active,
+            isDeleted: device.isDeleted,
+            lastValidLatitude: null,
+            lastValidLongitude: null,
+            engineOnAt: null,
+            engineOffAt: null,
+            engineStatusChangedAt: null,
+            speed: device.speed,
+            altitude: null,
+            engine: device.engine
+         }
+      });
+   }
+   catch (err) {
+      res.status(501).json({
+         status: 0
+      });
+   }
+
 }
 exports.getDevice = (req, res, next) => {
 
@@ -45,7 +54,7 @@ exports.updateDeviceLocation = async (req, res, next) => {
    try {
       const user = req.body.user;
       const { imei, latitude, longitude, speed, altitude } = req.body;
-      let device = await Device.findOne({ iemi: imei })
+      let device = await Device.findOne({ imei: imei })
       if (device === null) {
          return res.status(401).json({ status: 0 })
       }
@@ -54,7 +63,7 @@ exports.updateDeviceLocation = async (req, res, next) => {
       device.speed = speed;
       device.altitude = altitude;
       device.previousLocations = `${latitude},${longitude}`;
-      device.save();
+      let data = await device.save();
       return res.status(201).json({ status: 1 })
    }
    catch (err) {
